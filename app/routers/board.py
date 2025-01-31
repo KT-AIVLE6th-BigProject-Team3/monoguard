70e2e57f13bdd886025dd5b57cbbd013fb7f0970
Jaewon Chung <chjone0417@gmail.com>
2025-01-31 13:35:51 +0900

    qna page permission and alert

--- app/routers/board.py
+++ app/routers/board.py
@@ -107,8 +107,12 @@ def read_question(
 ):
     existing = db.query(QnA).filter(QnA.id == id).first()
     if not existing:
-        raise HTTPException(status_code=404, detail="존재하지 않는 QnA 게시글입니다.")
-   
+        return RedirectResponse(url=f"/board/qna/list?error=notexist", status_code=303)
+        # raise HTTPException(status_code=404, detail="존재하지 않는 QnA 게시글입니다.")
+    if not current_user['admin'] and current_user['sub'] != existing.user_id:
+        print(current_user['admin'])
+        return RedirectResponse(url=f"/board/qna/list?error=no_permission", status_code=303)
+        # raise HTTPException(status_code=403, detail="열람 권한이 없습니다.")
     qna_content = {
         "id" : existing.id,
         "user_id" : existing.user_id,
@@ -131,6 +135,41 @@ def read_question(
             "qna_content" : qna_content
         }
     )
+    
+# # 특정 게시글 조회
+# @router.get("/qna/content/{id}", response_class=HTMLResponse)
+# def read_question(
+#     request: Request,
+#     id: int,
+#     db: Session = Depends(lambda: SessionLocal()),
+#     current_user: dict = Depends(auth.get_current_user_from_cookie)
+# ):
+#     existing = db.query(QnA).filter(QnA.id == id).first()
+#     if not existing:
+#         raise HTTPException(status_code=404, detail="존재하지 않는 QnA 게시글입니다.")
+   
+#     qna_content = {
+#         "id" : existing.id,
+#         "user_id" : existing.user_id,
+#         "title" : existing.title,
+#         "content" : existing.content,
+#         "created_at" : existing.created_at,
+#         "reply_user" : existing.reply_user,
+#         "reply_title" : existing.reply_title,
+#         "reply_content" : existing.reply_content,
+#         "reply_at" : existing.reply_at,
+#         "filename": existing.attachment_filename,
+#         "content_type": existing.attachment_content_type,
+#         "current_user": current_user['sub'],
+#         "admin": current_user['admin']
+#     }
+#     return templates.TemplateResponse(
+#         "QnA_page.html",
+#         {
+#             "request" : request,
+#             "qna_content" : qna_content
+#         }
+#     )
 
 
 # QnA 첨부파일 다운로드    
